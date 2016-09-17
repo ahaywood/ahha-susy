5c60970f4fc6fdefacfbe17525ae88d9f0df2c07
John-Michael L'Allier <jmlallier@me.com>
2016-09-17 10:02:54 -0400

    Fix line 336 to add HTML5 Blank Custom Post Type

--- functions.php
+++ functions.php
@@ -333,7 +333,7 @@ add_action('wp_print_scripts', 'html5blank_conditional_scripts'); // Add Conditi
 add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comments
 add_action('wp_enqueue_scripts', 'html5blank_styles'); // Add Theme Stylesheet
 add_action('init', 'register_html5_menu'); // Add HTML5 Blank Menu
-on // Add our HTML5 Blank Custom Post Type
+add_action('init', 'create_post_type_html5'); // Add our HTML5 Blank Custom Post Type
 add_action('widgets_init', 'my_remove_recent_comments_style'); // Remove inline Recent Comment Styles from wp_head()
 add_action('init', 'html5wp_pagination'); // Add our HTML5 Pagination
 
